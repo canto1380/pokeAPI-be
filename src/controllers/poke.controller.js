35dc9765cc1e7f09ea006ed6bf6c25d6f2dcc38d
canto1380 <atapenalba16@gmail.com>
2022-11-28 00:38:29 -0300

    add route characteristic and paginate

--- src/controllers/poke.controller.js
+++ src/controllers/poke.controller.js
@@ -12,37 +12,54 @@ const getPokeByName = async (name) => {
 
 export const getPokemon = async (req, res) => {
   try {
+    const { page = 1, limit = 12, search = "" } = req.query;
+    const offset = (page - 1) * limit;
     await fetch(
-      `${process.env.REACT_APP_URL_API_POKE}/pokemon/?limit=10&offset=1`
+      `${process.env.REACT_APP_URL_API_POKE}/pokemon/?limit=${limit}&offset=${offset}`
     )
       .then((resp) => resp.json())
       .then(async (pokemon) => {
+        const { count } = pokemon;
+        const countPages = count / limit
         const promises = pokemon.results.map((poke) => {
           return getPokeByName(poke.name);
         });
         const pokeData = await Promise.all(promises);
-        signale.success('Successful query')
-        res.status(200).send(pokeData);
+        signale.success("Successful query");
+        res.status(200).json({ pokeData, countPages });
       });
   } catch (error) {
-    signale.error('API query error')
-    res.status(400).send({error: error.message, success: false})
+    signale.error("API query error");
+    res.status(400).send({ error: error.message, success: false });
   }
 };
 
 export const getPokemonById = async (req, res) => {
   try {
     const { id } = req.params;
-    await fetch(
-      `${process.env.REACT_APP_URL_API_POKE}/pokemon/${id}`
-    )
-    .then((resp) => resp.json())
-    .then(async (pokemon) => {
-      signale.success('Pokemon Found')
-      res.status(200).send(pokemon)
-    });
+    await fetch(`${process.env.REACT_APP_URL_API_POKE}/pokemon/${id}`)
+      .then((resp) => resp.json())
+      .then(async (pokemon) => {
+        signale.success("Pokemon Found");
+        res.status(200).send(pokemon);
+      });
   } catch (error) {
-    signale.error('Pokemon not found')
-    res.status(400).send({error: error.message, success: false})
+    signale.error("Pokemon not found");
+    res.status(400).send({ error: error.message, success: false });
+  }
+};
+
+export const characteristByPokemon = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await fetch(`${process.env.REACT_APP_URL_API_POKE}/characteristic/${id}`)
+      .then((resp) => resp.json())
+      .then(async (descriptions) => {
+        signale.success("Pokemon Found");
+        res.status(200).send(descriptions);
+      });
+  } catch (error) {
+    signale.error("Pokemon not found");
+    res.status(400).send({ error: error.message, success: false });
   }
 };
--- src/routes/poke.routes.js
+++ src/routes/poke.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import {getPokemon, getPokemonById} from "../controllers/poke.controller.js";
+import {characteristByPokemon, getPokemon, getPokemonById} from "../controllers/poke.controller.js";
 const router = Router()
 
 router.get('/', getPokemon)
 router.get('/:id', getPokemonById)
+router.get('/characteristic/:id', characteristByPokemon)
 
 export default router
